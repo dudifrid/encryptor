c9d8170f5c139ad19b56e686519af2449bcfc361
dudifrid <dudifrid@gmail.com>
2016-07-19 11:29:11 +0300

    Chapter 1

--- src/main/java/DavidFrid/encryptor/App.java
+++ src/main/java/DavidFrid/encryptor/App.java
@@ -1,10 +1,54 @@
 package DavidFrid.encryptor;
+import java.io.Console;
+import java.util.Scanner;
+import java.io.File;
 
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" ); 
+    	Scanner in = new Scanner(System.in);
+    	
+        System.out.println("Please press 'e' to use encryption, and press 'd' for decryption");
+        String input=null;
+        input = in.nextLine();
+        
+        boolean encryption=false;
+        if (input.equals("e"))
+        	encryption = true;
+        else if (input.equals("d"))
+        	encryption = false;
+        else
+        {
+        	System.err.println("Error: invalid input");
+        	System.exit(1);
+        }
+        
+        String path = null;
+        
+        while (true){
+        	System.out.println("Please enter source path");
+            path = input;
+            if (encryption)
+            	System.out.println("encryption simulation of file"+path);
+            else
+            	System.out.println("decryption simulation of file"+path);
+            File f = new File(path);
+            if (!f.exists())
+            {
+            	System.err.println("Error: File doesn't exist");
+            	continue;
+            }
+            if (!f.isFile())
+            {
+            	System.err.println("Error: not a file path");
+            	continue;
+            }
+            break;
+        }
+        
+        
     }
+    
 }
